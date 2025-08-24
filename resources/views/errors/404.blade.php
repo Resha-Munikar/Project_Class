381e13adbada633e918af1a189505798b08fc9ba
samyamzoc <zocsamyam34@gmail.com>
2025-08-24 09:23:18 +0545

    Updated

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 page not found</title>
+    @vite(['resources/js/app.js','resources/css/app.css'])
+</head>
+<body class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-4xl font-bold text-red-500">Page Not Found</h1>
+    <p class="mt-2">The page you are looking for is not available.</p>
+</body>
+</html>
